cbbf602c8b7ffe00d612851dbeb5ac4bd8660562
ahmedRaafat <beboofathi@gmail.com>
2020-07-21 22:48:20 +0200

    Add project files.

---
+++ ArmyTechTask/BL/GovernmentInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArmyTechTask.BL
+{
+    public class GovernmentInfo
+    {
+       public int govermentId { get; set; }
+    }
+}
---
+++ ArmyTechTask/Controllers/StudentsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ArmyTechTask.BL;
+using ArmyTechTask.Models;
+using PagedList;
+
+namespace ArmyTechTask.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private ArmyTechTaskEntities db = new ArmyTechTaskEntities();
+
+        // GET: Students
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 20;
+            int pageIndex = 1;
+
+            pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+
+
+
+            var students = db.Students.Include(s => s.Field).Include(s => s.Governorate).Include(s => s.Neighborhood).OrderBy(s => s.ID);
+            var st = students.ToPagedList(pageIndex, pageSize);
+
+            return View(st);
+        }
+
+        //// GET: Students/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = db.Students.Find(id);
+        //    if (student == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+
+        // GET: Students/Create
+
+
+
+        public ActionResult Create()
+        {
+            ViewBag.FieldId = new SelectList(db.Fields, "ID", "Name");
+            ViewBag.GovernorateId = new SelectList(db.Governorates, "ID", "Name");
+            ViewBag.NeighborhoodId = new SelectList(db.Neighborhoods, "ID", "Name");
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetNeighborhoodId(GovernmentInfo governmateInfo)
+        {
+
+            var Result = db.Neighborhoods.Where(n => n.GovernorateId == governmateInfo.govermentId).Select(x => new { Id = x.GovernorateId, Name = x.Name }).ToList();
+            return Json(new
+            {
+                res = Result
+            });
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,BirthDate,GovernorateId,NeighborhoodId,FieldId")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Edit", new { id = student.ID });
+            }
+
+            ViewBag.FieldId = new SelectList(db.Fields, "ID", "Name", student.FieldId);
+            ViewBag.GovernorateId = new SelectList(db.Governorates, "ID", "Name", student.GovernorateId);
+            ViewBag.NeighborhoodId = new SelectList(db.Neighborhoods, "ID", "Name", student.NeighborhoodId);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FieldId = new SelectList(db.Fields, "ID", "Name", student.FieldId);
+            ViewBag.GovernorateId = new SelectList(db.Governorates, "ID", "Name", student.GovernorateId);
+
+
+            ViewBag.NeighborhoodId = new SelectList(db.Neighborhoods, "ID", "Name", student.NeighborhoodId);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,BirthDate,GovernorateId,NeighborhoodId,FieldId")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FieldId = new SelectList(db.Fields, "ID", "Name", student.FieldId);
+            ViewBag.GovernorateId = new SelectList(db.Governorates, "ID", "Name", student.GovernorateId);
+            ViewBag.NeighborhoodId = new SelectList(db.Neighborhoods, "ID", "Name", student.NeighborhoodId);
+            return View(student);
+        }
+
+        //// GET: Students/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = db.Students.Find(id);
+        //    if (student == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+
+        // POST: Students/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+
+            return Json(new
+            {
+                msg = "Successfully Deleted :" + student.Name
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
